35ce0859602c12d3e11b7f582d116570e6424380
Кузнецова Надежда Вадимовна <kuznetsovanadja@ws06>
2016-03-21 17:25:36 +0300

    texture loads

--- js/main.js
+++ js/main.js
@@ -1,34 +1,29 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
-renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
+renderer.setClearColor( 0xffffff );
 $(".content").append(renderer.domElement);
-var geometry = new THREE.SphereGeometry( 5, 32, 32 );
-var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-//var sphere = new THREE.Mesh( geometry, material );
-var sphere = createMesh(new THREE.SphereGeometry(5, 20, 20), "sun.jpg");
-scene.add( sphere );
-camera.position.z = 30;
+camera.position.z = 300;
+
+var loader = new THREE.TextureLoader();
+loader.load( 'img/sun.jpg', function ( texture ) {
+
+    var geometry = new THREE.SphereGeometry( 100, 20, 20 );
+    var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
+    var mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+} );
 
 function render() {
     requestAnimationFrame( render );
 
-    //sphere.rotation.x += 0.1;
-    //sphere.rotation.y += 0.1;
+    scene.rotation.y -= 0.005;
 
     renderer.render( scene, camera );
 }
 
 render();
-
-function createMesh(geom, imageFile) {
-    var texture = THREE.ImageUtils.loadTexture("./img/" + imageFile);
-    var mat = new THREE.MeshPhongMaterial();
-    mat.map = texture;
-
-    var mesh = new THREE.Mesh(geom, mat);
-    return mesh;
-}
 
 function change (){
 }
