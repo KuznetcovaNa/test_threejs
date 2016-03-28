34ce3551d25487b041383be57ab9645bbf086c40
Кузнецова Надежда Вадимовна <kuznetsovanadja@ws06>
2016-03-28 16:31:19 +0300

    control panel

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
+$("#control_r_middle").change(function(){
+    scene.remove(mesh);
+    loader.load( 'img/sun.jpg', function ( texture ) {
+
+        var geometry = new THREE.SphereGeometry($("#control_r_middle").val() , 32, 32 );
+        var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
+        mesh = new THREE.Mesh( geometry, material );
+        scene.add( mesh );
+
+    } );
+});
 var scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 2000 );
-camera.position.z = 500;
+camera.position.z = 100;
 var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor( 0xffffff );
+renderer.setSize(window.innerWidth, window.innerHeight);
 $(".content").append(renderer.domElement);
 var mesh;
 var sphere;
 var loader = new THREE.TextureLoader();
 loader.load( 'img/sun.jpg', function ( texture ) {
 
-    var geometry = new THREE.SphereGeometry( 10, 20, 20 );
+    var geometry = new THREE.SphereGeometry( 10, 32, 32 );
     var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
@@ -17,26 +29,28 @@ loader.load( 'img/sun.jpg', function ( texture ) {
 } );
 loader.load( 'img/polka_dot.jpg', function ( texture ) {
 
-    var sphereGeometry = new THREE.SphereGeometry(2, 20, 20);
+    var sphereGeometry = new THREE.SphereGeometry(5, 32, 32);
     var sphereMaterial = new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 });
     sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    sphere.position.x = 0;
+    sphere.position.x = 20;
     sphere.position.y = 0;
-    sphere.position.z = 10;
+    sphere.position.z = 0;
     scene.add(sphere);
 
 } );
+
+
 var camera_controls = new THREE.TrackballControls(camera);
 camera_controls.target.set(0, 0, 0);
 var step = 0;
 function render() {
     requestAnimationFrame( render );
 
-    mesh.rotation.y -= 0.005;
-    sphere.rotation.y -= 0.005;
+    mesh.rotation.y += 0.005;
+    sphere.rotation.y -= 0.01;
     step += 0.01;
-    sphere.position.x = 15*Math.sin(step+Math.PI/2);
-    sphere.position.y = 15*Math.sin(step);
+    sphere.position.x = 20*Math.sin(step+Math.PI/2);
+    sphere.position.z = 20*Math.sin(step);
 
     camera_controls.update();
 
@@ -44,6 +58,3 @@ function render() {
 }
 
 render();
-
-function change (){
-}
